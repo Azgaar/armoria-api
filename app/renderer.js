5d83efd7dd7239cb9f665a8451be6d394978add9
Azgaar <maxganiev@yandex.ru>
2021-02-26 01:22:30 +0300

    transform update

--- app/renderer.js
+++ app/renderer.js
@@ -92,7 +92,7 @@ async function draw(id, coa, size, colors) {
 
   function templateOrdinary(ordinary, tincture) {
     const fill = clr(tincture);
-    let svg = `<g fill="${fill}" stroke="none">`;
+    let svg = `<g fill="${fill}" stroke="none" transform="${transform(ordinary)}">`;
     if (ordinary.ordinary === "bordure") svg += `<path d="${shieldPath}" fill="none" stroke="${fill}" stroke-width="16.7%"/>`;
     else if (ordinary.ordinary === "orle") svg += `<path d="${shieldPath}" fill="none" stroke="${fill}" stroke-width="5%" transform="translate(15 15) scale(.85)"/>`;
     else svg += getTemplate(ordinary.ordinary, ordinary.line);
@@ -104,23 +104,46 @@ async function draw(id, coa, size, colors) {
     const chargePositions = [...new Set(charge.p)].filter(position => positions[position]);
 
     let svg = "";
-    svg += `<g fill="${fill}" stroke="#000">`;
+    svg += `<g fill="${fill}" stroke="#000" transform="${transform(charge)}">`;
     for (const p of chargePositions) {
       const transform = getElTransform(charge, p);
-      svg += `<use xlink:href="#${charge.charge}_${id}" transform="${transform}"></use>`;
+      svg += `<use xlink:href="#${charge.charge}_${id}" transform="${transform}"/>`;
     }
     return svg + `</g>`;
+  }
 
-    function getElTransform(c, p) {
-      const s = Math.round((c.size || 1) * sizeModifier * 100) / 100;
-      const sx = c.sinister ? -s : s;
-      const sy = c.reversed ? -s : s;
-      let [x, y] = positions[p];
-      x = x - 100 * (sx - 1);
-      y = y - 100 * (sy - 1);
-      const scale = c.sinister || c.reversed ? `${sx} ${sy}` : s;
-      return `translate(${x} ${y}) scale(${scale})`;
+  function round(n) {
+    return Math.round(n * 100) / 100;
+  }
+
+  function getElTransform(c, p) {
+    const s = round((c.size || 1) * sizeModifier);
+    const sx = c.sinister ? -s : s;
+    const sy = c.reversed ? -s : s;
+    let [x, y] = positions[p];
+    x = round(x - 100 * (sx - 1));
+    y = round(y - 100 * (sy - 1));
+
+    const translate = x || y ? `translate(${x} ${y})` : null;
+    const scale = sx !== 1 || sy !== 1 ? sx === sy ? `scale(${s})` : `scale(${sx} ${sy})` : null;
+    return translate && scale ? `${translate} ${scale}` : translate ? translate : scale ? scale : null;
+  }
+
+  function transform(charge) {
+    let {x = 0, y = 0, angle = 0, size = 1, p} = charge;
+    if (p) size = 1; // size is defined on use element level
+  
+    if (size !== 1) {
+      x = round(x + 100 - size * 100);
+      y = round(y + 100 - size * 100);
     }
+  
+    let transform = "";
+    if (x || y) transform += `translate(${x} ${y})`;
+    if (angle) transform += ` rotate(${angle} ${size * 100} ${size * 100})`;
+    if (size !== 1) transform += ` scale(${size})`;
+  
+    return transform ? transform.trim() : null;
   }
 
   function getPatterns(coa, id) {
