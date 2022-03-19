9b2d2e763bd1ffe2a4860d118f0ce617a8ae7d5e
safaa-alnabulsi <safaa.alnablsi@gmail.com>
2022-03-19 18:57:30 +0100

    Push the node_modules as npm will not be installedgit status

---
+++ node_modules/svg2img/index.d.ts
@@ -0,0 +1,20 @@
+type Callback = (err: any, buffer: any) => any;
+
+declare enum Format {
+  jpeg = 'jpeg',
+  jpg = 'jpg',
+  png = 'png',
+}
+
+export interface svg2imgOptions {
+  width?: number;
+  height?: number;
+  preserveAspectRatio?: boolean | string;
+  format?: Format;
+  quality?: number;
+  compressionLevel?: number;
+}
+
+declare function svg2img(svg: string, options: svg2imgOptions, callback: Callback): void;
+declare function svg2img(svg: string, callback: Callback): void;
+export default svg2img;
